e9432d0ef2bc474bb335c056e43332c15ef125f8
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2021-06-17 15:12:35 +0700

    update

--- admin.php
+++ admin.php
@@ -29,7 +29,7 @@
           <a class="navbar-brand brand-logo" href="admin.php">
             <img src="images/logo.svg" alt="logo" class="logo-dark" />
           </a>
-          <a class="navbar-brand brand-logo-mini" href="admin.php"><img src="images/logo-mini.svg" alt="logo" /></a>
+          
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center flex-grow-1">
           <h5 class="mb-0 font-weight-medium d-none d-lg-flex">Giao diện nhân viên</h5>
